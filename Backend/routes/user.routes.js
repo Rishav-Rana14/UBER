be80c9be95e8733843b6d72c132b08e5fb1f65ef
Rishav Rana <rishavrana@externlabs.com>
2024-12-19 13:10:12 +0530

    update: registration done

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller.js');
 const {body} = require('express-validator')
+const authMiddleware = require('../middleware/auth.middleware.js');
 
 router.post('/register', [
     body('email').isEmail().withMessage('Invalid Email'),
@@ -10,5 +11,15 @@ router.post('/register', [
 ],
     userController.registerUser
 )
+
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+],
+    userController.loginUser
+)
+
+router.get('/getUser', authMiddleware.authUser, userController.getUser);
+router.get('/logout', authMiddleware.authUser, userController.logoutUser)
 
 module.exports = router
