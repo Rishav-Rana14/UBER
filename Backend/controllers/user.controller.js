aadb7be2522e8600096c4aa7a87022c14eaa4692
Rishav Rana <rishavrana@externlabs.com>
2024-12-19 12:34:29 +0530

    update: user

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/user.model.js');
 const userService = require('../services/user.service.js')
 const {validationResult} = require('express-validator')
+const blackListTokenModel = require('../models/blacklistToken.model.js');
 
 module.exports.registerUser = async (req, res, next) => {
 
@@ -27,5 +28,54 @@ module.exports.registerUser = async (req, res, next) => {
     });
     const token = user.generateAuthToken();
     res.status(201).json({ token, user });
+
+}
+
+module.exports.loginUser = async (req, res, next) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ message: errors.array().map(err => err.msg).join('; ') });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if(!user){
+        return res.status(400).json({ message: 'User not found' });  
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if(!isMatch){
+        return res.status(400).json({ message: 'Invalid email or password' });
+    }
+    const token = user.generateAuthToken();
+    res.cookie("token", token);
+    res.status(200).json({ 
+        token, 
+         user: {
+             id: user._id,
+             email: user.email,
+             fullname: {
+                 firstname: user.fullname.firstname,
+                 lastname: user.fullname.lastname
+             }
+         }
+    });
+} 
+
+module.exports.getUser = async( req, res, next ) =>{
+    res.status(200).json(req.user);
+}
+
+module.exports.logoutUser = async (req, res, next) => {
+    res.clearCookie('token');
+    const token = req.cookies?.token || req.headers.authorization.split(' ')[ 1 ];
+
+    await blackListTokenModel.create({ token });
+
+    res.status(200).json({ message: 'Logged out' });
 
 }
